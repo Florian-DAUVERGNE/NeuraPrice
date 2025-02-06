5a52ee4140c09db24e5b4ba4f4f66d889455cd89
Florian-DAUVERGNE <florian.dauv@gmail.com>
2025-02-06 21:56:52 +0100

    feat: added car features

--- Frontend/app/car/page.tsx
+++ Frontend/app/car/page.tsx
@@ -7,24 +7,98 @@ import PhoneFormData from "@/types/PhoneFormData.interface";
 import { Brand,Conditions } from "@/types/enums/phone.enum";
 import { useFormSubmit } from "@/hooks/useFormSubmit";
 
+
+const marque = [
+  "Renault",
+  "Peugeot",
+  "BMW",
+  "Audi",
+  "Mercedes",
+  "Volkswagen",
+  "Toyota",
+  "Citroen",
+  "Ford",
+  "Tesla",
+  "Dacia",
+  "Hyundai",
+  "Kia",
+  "Kia",
+  "Porsche",
+  "Nissan"
+]
+
+const carburants = ["Essence", "Diesel", "Electrique", "Hybride"]
+const boites_vitesse = ["Manuelle", "Automatique"]
+const types_vehicules = ["Berline", "Break", "SUV", "Coupé", "Monospace", "Pick-up"]
+const pays_origine = ["France", "Allemagne", "Suede", "Italie", "Espagne", "Belgique", "Royaume-Uni", "Autriche"]
+const couleurs = ["Noir", "Blanc", "Gris", "Bleu", "Rouge", "Vert", "Jaune", "Argent", "Orange"]
+const critaires_air = ["Euro 4", "Euro 5", "Euro 6", "Euro 6", "Euro 7", "Euro 8"]
+const classes_co2 = ["A", "B", "C", "D", "E"]
+const types_puissance = ["65", "75", "90", "95", "110", "120", "130", "150", "200"]
+const n_places = ["2", "5", "7"]
+
+
+
+
+
 export default function PhoneForm() {
   const { handleFormSubmit } = useFormSubmit();
 
   const fields: FormField[] = [
     {
-      name: "brand",
+      name: "Marque",
       label: "Marque",
       type: "select",
       placeholder: "Sélectionnez la marque",
-      options: Object.values(Brand),
+      options: marque,
       required: true,
     },
     {
-      name: "condition",
-      label: "État",
+      name: "Km",
+      label: "Kilométrage",
+      type: "input",
+      placeholder: "Saisissez le kilométrage",
+      required: true,
+    },
+    {
+      name: "Carburant",
+      label: "Carburant",
       type: "select",
-      placeholder: "Sélectionnez l'état",
-      options: Object.values(Conditions),
+      placeholder: "Sélectionnez le carburant",
+      options: carburants,
+      required: true,
+    },
+    {
+      name: "Boite_vitesse",
+      label: "Boite de vitesse",
+      type: "select",
+      placeholder: "Sélectionnez la boite de vitesse",
+      options: boites_vitesse,
+      required: true,
+    },
+    {
+      name: "Puissance",
+      label: "Puissance",
+      type: "select",
+      placeholder: "Saisissez la puissance",
+      options: types_puissance,
+      required: true,
+    },    
+
+    {
+      name: "Critere_air",
+      label: "Critere'air",
+      type: "select",
+      placeholder: "Saisissez le critere'air",
+      required: true,
+
+
+    },
+    {
+      name: "Couleur",
+      label: "Couleur",
+      type: "select",
+      placeholder: "Saisissez la couleur",
       required: true,
     },
   ];
