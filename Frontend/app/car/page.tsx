014923db4420005ded7f5133aa98705c0c731467
Florian-DAUVERGNE <florian.dauv@gmail.com>
2025-02-08 19:10:32 +0100

    refactor: implemented params

--- Frontend/app/car/page.tsx
+++ Frontend/app/car/page.tsx
@@ -4,38 +4,24 @@ import DynamicForm from "@/components/basic/DynamicForm";
 import { Car } from "lucide-react";
 import FormField from "@/types/FormField.interface";
 import PhoneFormData from "@/types/PhoneFormData.interface";
-import { Brand,Conditions } from "@/types/enums/phone.enum";
+//import { Brand,Conditions } from "@/types/enums/phone.enum";
 import { useFormSubmit } from "@/hooks/useFormSubmit";
 
 
-const marque = [
-  "Renault",
-  "Peugeot",
-  "BMW",
-  "Audi",
-  "Mercedes",
-  "Volkswagen",
-  "Toyota",
-  "Citroen",
-  "Ford",
-  "Tesla",
-  "Dacia",
-  "Hyundai",
-  "Kia",
-  "Kia",
-  "Porsche",
-  "Nissan"
-]
+const marque = ['Tesla', 'Hyundai', 'BMW', 'Peugeot', 'Renault', 'Dacia', 'Mercedes', 'Toyota', 'Porsche', 'Citroen', 'Kia', 'Nissan',
+  'Volkswagen', 'Ford', 'Audi']
 
-const carburants = ["Essence", "Diesel", "Electrique", "Hybride"]
+const carburants = ['Electrique', 'Diesel', 'Gaz', 'Essence', 'Hybride']
 const boites_vitesse = ["Manuelle", "Automatique"]
-const types_vehicules = ["Berline", "Break", "SUV", "Coupé", "Monospace", "Pick-up"]
-const pays_origine = ["France", "Allemagne", "Suede", "Italie", "Espagne", "Belgique", "Royaume-Uni", "Autriche"]
-const couleurs = ["Noir", "Blanc", "Gris", "Bleu", "Rouge", "Vert", "Jaune", "Argent", "Orange"]
-const critaires_air = ["Euro 4", "Euro 5", "Euro 6", "Euro 6", "Euro 7", "Euro 8"]
-const classes_co2 = ["A", "B", "C", "D", "E"]
-const types_puissance = ["65", "75", "90", "95", "110", "120", "130", "150", "200"]
-const n_places = ["2", "5", "7"]
+
+const critaires_air = ['Euro 1', 'Euro 3', 'Euro 6', 'Euro 4', 'Euro 5']
+
+const couleurs = ['Bleu', 'Noir', 'Gris', 'Blanc', 'Rouge']
+
+const pays = ['Japon', 'USA', 'Espagne', 'France', 'Allemagne', 'Italie']
+
+const types= ['Break', 'SUV', 'Monospace', 'Crossover', 'Berline']
+
 
 
 
@@ -79,9 +65,8 @@ export default function PhoneForm() {
     {
       name: "Puissance",
       label: "Puissance",
-      type: "select",
+      type: "input",
       placeholder: "Saisissez la puissance",
-      options: types_puissance,
       required: true,
     },    
 
@@ -90,17 +75,43 @@ export default function PhoneForm() {
       label: "Critere'air",
       type: "select",
       placeholder: "Saisissez le critere'air",
+      options: critaires_air,
       required: true,
-
-
     },
     {
       name: "Couleur",
       label: "Couleur",
       type: "select",
       placeholder: "Saisissez la couleur",
+      options: couleurs,
+      required: true,
+
+    },
+    {
+      name: "Type_vehicule",
+      label: "Type de vehicule",
+      type: "select",
+      placeholder: "Saisissez le type de vehicule",
+      options: types,
       required: true,
     },
+    {
+      name: "Pays",
+      label: "Pays d'origine",
+      type: "select",
+      placeholder: "Saisissez le pays d'origine",
+      options: pays,
+      required: true,
+    },
+    {
+      name: "age",
+      label: "Age",
+      type: "input",
+      placeholder: "Saisissez l'age",
+      required: true,
+    },
+
+
   ];
 
   return (
